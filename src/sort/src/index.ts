2595de6073d852558e112dbd869e66c01c06ba39
Emil <emillibera@gmail.com>
2020-07-29 21:01:31 +0200

    initial stats

--- src/sort/src/index.ts
+++ src/sort/src/index.ts
@@ -2,7 +2,15 @@ import { NumbersCollection } from "./NumbersCollection";
 import { Sorter } from "./Sorter";
 import {LinkedList} from "./LinkedList";
 
-const sorter = new Sorter(new NumbersCollection([2,3,4,5,5,1,2,3,-2]))
-// const linen = new Sorter(new LinkedList())
-console.log(sorter.sort());
-console.log(sorter.collection);
+console.log("Numbers Collection")
+const numbersCollections = new NumbersCollection([2,3,4,5,5,1,2,3,-2])
+numbersCollections.sort()
+console.log(numbersCollections)
+
+console.log("linkedList")
+const linkedList = new LinkedList()
+linkedList.add(34)
+linkedList.add(23)
+linkedList.add(32)
+linkedList.sort()
+linkedList.print()
